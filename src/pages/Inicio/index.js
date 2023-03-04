fb3fcd35197d86e12c15c502ee4066e94ffc1f9f
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-03-04 20:52:41 -0300

    add absolute import

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,11 @@
+import Banner from "../../components/Banner";
 
 export default function Inicio() {
     return (
-      <div>Pagina inicial.</div>
+      <main>
+        <Banner />
+        <div>Pagina inicial.</div>
+      </main>
     )
   }
   
